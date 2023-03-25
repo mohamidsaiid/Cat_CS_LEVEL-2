5cd4733738fd5642b56f2ab3ecb14315fb4abe6d
mohamidsaiid <100053790+mohamidsaiid@users.noreply.github.com>
2023-03-25 23:46:54 +0200

    Update Maximizing XOR.cpp

--- bit manipulation/hacker rank/Maximizing XOR.cpp
+++ bit manipulation/hacker rank/Maximizing XOR.cpp
@@ -1,19 +1,3 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
-string ltrim(const string &);
-string rtrim(const string &);
-
-/*
- * Complete the 'maximizingXor' function below.
- *
- * The function is expected to return an INTEGER.
- * The function accepts following parameters:
- *  1. INTEGER l
- *  2. INTEGER r
- */
-
 int maximizingXor(int l, int r) {
     int x = l ^ r;
     int c = 0;
@@ -23,49 +7,4 @@ int maximizingXor(int l, int r) {
         x >>= 1;
     }
     return ((1<<c) - 1);
-}
-
-int main()
-{
-    ofstream fout(getenv("OUTPUT_PATH"));
-
-    string l_temp;
-    getline(cin, l_temp);
-
-    int l = stoi(ltrim(rtrim(l_temp)));
-
-    string r_temp;
-    getline(cin, r_temp);
-
-    int r = stoi(ltrim(rtrim(r_temp)));
-
-    int result = maximizingXor(l, r);
-
-    fout << result << "\n";
-
-    fout.close();
-
-    return 0;
-}
-
-string ltrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        s.begin(),
-        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
-    );
-
-    return s;
-}
-
-string rtrim(const string &str) {
-    string s(str);
-
-    s.erase(
-        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
-        s.end()
-    );
-
-    return s;
 }
